02dd5ae49dd68d56497266ce71be50f7ed5213a2
quasemortx <96626993+quasemortx@users.noreply.github.com>
2025-11-26 18:37:49 -0300

    Story BE-02-Completo

--- frontend/lib/core/api.dart
+++ frontend/lib/core/api.dart
@@ -8,13 +8,27 @@ class Api {
     required String agentId,
     required String prompt,
   }) async {
-    final response = await http.post(
-      Uri.parse("$baseUrl/agents/$agentId/execute"),
-      headers: {"Content-Type": "application/json"},
-      body: json.encode({"message": prompt}),
-    );
+    print("agent_id: $agentId , prompt: $prompt");
+    try {
+      final response = await http.post(
+        Uri.parse("$baseUrl/agents/$agentId/execute"),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({"message": prompt}),
+      );
 
-    final data = json.decode(response.body);
-    return data["response"];
+      if (response.statusCode != 200) {
+        print(
+          "erro de timeout ou falha do gemini: Status ${response.statusCode} - ${response.body}",
+        );
+        throw Exception("Erro na API: ${response.statusCode}");
+      }
+
+      final data = json.decode(response.body);
+      print("Resposta do agent_id ao cliente ${data["response"]}");
+      return data["response"];
+    } catch (e) {
+      print("erro de timeout ou falha do gemini: $e");
+      rethrow;
+    }
   }
 }
